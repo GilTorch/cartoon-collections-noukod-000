d3a3a12e734e78374a5247ae84cdefd018a77eda
Gilbert Torchon <gilbert@noukod.com>
2018-09-19 19:01:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,4 +29,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|element| element=="cheddar" || element=="gouda" || element=="camembert"}
 end
