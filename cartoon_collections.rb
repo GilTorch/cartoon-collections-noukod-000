f7d2a2f2795a6408b4cfb37f74c9b3f2ed3ca245
Gilbert Torchon <gilbert@noukod.com>
2018-09-19 18:44:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,13 +14,14 @@ def long_planeteer_calls(array)
   # Your code here
   return_values = []
   array.each {|element|
-    return_values << element>=4
-    if element>=4
+    return_values << (element.length>=4)
+    if element.length>=4
       return true
     end
   }
   return_values.all?{|element| element==true }
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
