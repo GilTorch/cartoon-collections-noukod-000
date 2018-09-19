b70cdc7fac911056137a4fe5352022e5312fa35e
Gilbert Torchon <gilbert@noukod.com>
2018-09-19 18:18:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,11 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|dwarf,index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.collect {|element|
+    element.capitalize+"!"
+  }
 end
 
 def long_planeteer_calls# code an argument here
