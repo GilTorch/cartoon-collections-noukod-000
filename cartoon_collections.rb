a4786d356618135c23d1f40ac813e0704fad1d61
Gilbert Torchon <gilbert@noukod.com>
2018-09-19 18:21:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,14 @@ def summon_captain_planet(array)
   }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.each {|element|
+    if(element.length>=4)
+      return true
+    end
+  }
+  return false
 end
 
 def find_the_cheese# code an argument here
